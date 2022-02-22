8ae3ab47e8a4ad6f9f58e35d507dd8838ef8d3d5
Jon Naske <jonnaske@hotmail.com>
2022-02-21 16:18:09 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MathLoops
+
+Arithemtic game for elementary school students.
+
+Written in React and TypeScript.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+import {Operator} from './types';
+
+function App() {
+  const [opA, setOpA] = useState(0);
+  const [opB, setOpB] = useState(0);
+  const [operator, setOperator] = useState('');
+
+  const pickNumbers = () => {};
+  return <div className="App">this is a div</div>;
+}
+
+export default App;
