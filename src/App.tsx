dc76e8fab8d6168789fe918408d9e56f26c594bc
Jon Naske <jonnaske@hotmail.com>
2022-02-21 18:09:52 -0800

    added lodash, basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {Operator} from './types';
-
+import {Button, Container, TextField} from '@mui/material';
+import {random} from 'lodash';
 function App() {
   const [opA, setOpA] = useState(0);
   const [opB, setOpB] = useState(0);
-  const [operator, setOperator] = useState('');
+  const [operator, setOperator] = useState('add');
+  const [result, setResult] = useState(0);
 
-  const pickNumbers = () => {};
-  return <div className="App">this is a div</div>;
+  const determineResult = (): number => {
+    switch (operator) {
+      case 'add':
+        return opA + opB;
+      default:
+        return 0;
+    }
+  };
+  const pickNumbers = (): number => {
+    return random(1, 50);
+  };
+
+  useEffect(() => {
+    setOpA(pickNumbers);
+    setOpB(pickNumbers);
+    setResult(determineResult);
+  }, [opA, opB, result, operator]);
+  return (
+    <Container>
+      <h1>
+        {opA} {operator} {opB}
+      </h1>
+      <TextField id="answer" label="Enter Answer" variant="outlined" />
+      <Button>Submit Answer</Button>
+    </Container>
+  );
 }
 
 export default App;
